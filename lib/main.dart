940ea4a5dc06209a6008cd54a2e5292867b543df
JeetJhaveri <jeetwork941@gmail.com>
2023-12-27 01:34:41 +0530

    some dependencies added

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,7 @@ import 'package:demo/app/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const App()); // This is the main widget which will run in the application
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(
+      const App()); // This is the main widget which will run in the application
 }
